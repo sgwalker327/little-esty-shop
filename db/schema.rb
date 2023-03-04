fcf21b968c60945656d252e4e1dc8980ea6f1b25
Sam Walker <sgwalker327@gmail.com>
2023-03-04 12:52:08 -0700

    Modify last migration and add timestamps to bulk discounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_04_192046) do
+ActiveRecord::Schema.define(version: 2023_03_04_193813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bulk_discounts", force: :cascade do |t|
     t.bigint "merchant_id"
-    t.float "discount"
+    t.float "percent"
     t.integer "quantity_threshold"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["merchant_id"], name: "index_bulk_discounts_on_merchant_id"
   end
 
