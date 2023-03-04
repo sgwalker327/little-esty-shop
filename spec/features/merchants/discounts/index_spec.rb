820db96cec0e7394d3681b0e664621aa63f11be6
Sam Walker <sgwalker327@gmail.com>
2023-03-04 16:32:37 -0700

    Write initial test for merchant/discount index page

--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'merchant/:id/discounts', type: :feature do
+  before do
+    @merchant21 = create(:merchant)
+    @merchant22 = create(:merchant)
+  end
+
   describe 'when I visit the merchant discocunt index page' do
-    it ''
+    it 'displays the available bulk discounts and attributes' do
+      discount1 = @merchant21.discounts.create!(merchant_id: @merchant21.id, percent: 0.2, threshold: 10)
+      discount2 = @merchant21.discounts.create!(merchant_id: @merchant21.id, percent: 0.33, threshold: 20)
+      discount3 = @merchant22.discounts.create!(merchant_id: @merchant22.id, percent: 0.40, threshold: 30)
+      visit merchant_discounts_path(@merchant21)
+
+      expect(page).to have_content("Available Bulk Discounts for #{@merchant21.name}")
+      expect(page).to have_content("#{discount1.percent}% off #{discount1.threshold} or more items")
+      expect(page).to have_content("#{discount2.percent}% off #{discount2.threshold} or more items")
+      expect(page).to_not have_content("#{discount3.percent}% off #{discount3.threshold} or more items")
+    end
   end
 end
