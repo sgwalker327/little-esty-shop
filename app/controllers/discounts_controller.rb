1d7f6d9fca80ef07a781e1afa633c16e0ee9a135
Sam Walker <sgwalker327@gmail.com>
2023-03-04 21:31:57 -0700

    add edit discount button to view and added merchant to show controller action

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -2,10 +2,12 @@ class DiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
     @discounts = @merchant.discounts
+    
   end
 
   def show
     @discount = Discount.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
   
   end
 
