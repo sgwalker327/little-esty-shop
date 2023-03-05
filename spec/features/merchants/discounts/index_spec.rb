47473a0d892e1083c84a589d59d7132a92ed1699
Sam Walker <sgwalker327@gmail.com>
2023-03-04 18:17:10 -0700

    Complete test for button redirection

--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -2,18 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'merchant/:id/discounts', type: :feature do
   before do
-    
-  end
-
-  describe 'when I visit the merchant discount index page' do
-    it 'displays the available bulk discounts and attributes' do
     @merchant21 = create(:merchant)
     @merchant22 = create(:merchant)
     @discount1 = @merchant21.discounts.create!(merchant_id: @merchant21.id, percent: 0.2, threshold: 10)
     @discount2 = @merchant21.discounts.create!(merchant_id: @merchant21.id, percent: 0.33, threshold: 20)
     @discount3 = @merchant22.discounts.create!(merchant_id: @merchant22.id, percent: 0.40, threshold: 30)
-      visit merchant_discounts_path(@merchant21)
+    visit merchant_discounts_path(@merchant21)
+  end
 
+  describe 'when I visit the merchant discount index page' do
+    it 'displays the available bulk discounts and attributes' do
       expect(page).to have_content("Available Bulk Discounts for #{@merchant21.name}")
       expect(page).to have_content("#{@discount1.percent}% off #{@discount1.threshold} or more items")
       expect(page).to have_content("#{@discount2.percent}% off #{@discount2.threshold} or more items")
@@ -21,14 +19,6 @@ RSpec.describe 'merchant/:id/discounts', type: :feature do
     end
     
     it 'has a link to each discount\'s show page' do
-      @merchant21 = create(:merchant)
-      @merchant22 = create(:merchant)
-      @discount1 = @merchant21.discounts.create!(merchant_id: @merchant21.id, percent: 0.2, threshold: 10)
-      @discount2 = @merchant21.discounts.create!(merchant_id: @merchant21.id, percent: 0.33, threshold: 20)
-      @discount3 = @merchant22.discounts.create!(merchant_id: @merchant22.id, percent: 0.40, threshold: 30)
-
-      visit merchant_discounts_path(@merchant21)
-      
       within "#discount-#{@discount1.id}" do
         expect(page).to have_link("View Discount")
         click_on "View Discount"
@@ -38,15 +28,15 @@ RSpec.describe 'merchant/:id/discounts', type: :feature do
     end
 
     it 'has a link to create a new Discount' do
-      @merchant21 = create(:merchant)
-      @merchant22 = create(:merchant)
-      @discount1 = @merchant21.discounts.create!(merchant_id: @merchant21.id, percent: 0.2, threshold: 10)
-      @discount2 = @merchant21.discounts.create!(merchant_id: @merchant21.id, percent: 0.33, threshold: 20)
-      @discount3 = @merchant22.discounts.create!(merchant_id: @merchant22.id, percent: 0.40, threshold: 30)
-
-      visit merchant_discounts_path(@merchant21)
 
       expect(page).to have_link("Create Discount")
+
+    end
+
+    it 'when I click the link I am redirected a new discount page' do
+      click_on "Create Discount"
+
+      expect(current_path).to eq(new_merchant_discount_path(@merchant21))
     end
   end
 end
