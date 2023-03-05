994f56fb9a89fdda4d6b912537527ba6f61e46a4
Sam Walker <sgwalker327@gmail.com>
2023-03-04 17:55:24 -0700

    Write test to check for Create Discount link

--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -26,14 +26,27 @@ RSpec.describe 'merchant/:id/discounts', type: :feature do
       @discount1 = @merchant21.discounts.create!(merchant_id: @merchant21.id, percent: 0.2, threshold: 10)
       @discount2 = @merchant21.discounts.create!(merchant_id: @merchant21.id, percent: 0.33, threshold: 20)
       @discount3 = @merchant22.discounts.create!(merchant_id: @merchant22.id, percent: 0.40, threshold: 30)
+
       visit merchant_discounts_path(@merchant21)
-      save_and_open_page
+      
       within "#discount-#{@discount1.id}" do
         expect(page).to have_link("View Discount")
         click_on "View Discount"
       end
 
       expect(current_path).to eq("/merchants/#{@merchant21.id}/discounts/#{@discount1.id}")
+    end
+
+    it 'has a link to create a new Discount' do
+      @merchant21 = create(:merchant)
+      @merchant22 = create(:merchant)
+      @discount1 = @merchant21.discounts.create!(merchant_id: @merchant21.id, percent: 0.2, threshold: 10)
+      @discount2 = @merchant21.discounts.create!(merchant_id: @merchant21.id, percent: 0.33, threshold: 20)
+      @discount3 = @merchant22.discounts.create!(merchant_id: @merchant22.id, percent: 0.40, threshold: 30)
+
+      visit merchant_discounts_path(@merchant21)
+
+      expect(page).to have_link("Create Discount")
     end
   end
 end
