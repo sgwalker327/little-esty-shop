27f8ddad319253c797c60aa9391b6f305fdec6eb
Sam Walker <sgwalker327@gmail.com>
2023-03-04 17:21:50 -0700

    Add show action to discounts controller

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -3,6 +3,10 @@ class DiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @discounts = @merchant.discounts
     # require 'pry'; binding.pry
+  end
+
+  def show
+    @discount = Discount.find(params[:id])
     # require 'pry'; binding.pry
   end
 end
